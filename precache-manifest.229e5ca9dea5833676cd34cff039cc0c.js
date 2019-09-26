dfd4457842046fe516fcbcac3e5771b7296760c9
ihopeyouwin <ihopeyouwon@gmail.com>
2019-09-26 03:54:45 +0300

    Updates

---
+++ precache-manifest.229e5ca9dea5833676cd34cff039cc0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03da1ef62a3d3d3e9adf3bb7ddb78f1",
+    "url": "/ws_react_chat/index.html"
+  },
+  {
+    "revision": "ef24b6eb20a0d7cab471",
+    "url": "/ws_react_chat/static/css/main.716fb356.chunk.css"
+  },
+  {
+    "revision": "20773dd1ee9dfda1d4b0",
+    "url": "/ws_react_chat/static/js/2.b9b11b5b.chunk.js"
+  },
+  {
+    "revision": "ef24b6eb20a0d7cab471",
+    "url": "/ws_react_chat/static/js/main.a658df53.chunk.js"
+  },
+  {
+    "revision": "312b6a6b82bc59626da7",
+    "url": "/ws_react_chat/static/js/runtime-main.ecf85c37.js"
+  }
+]);
